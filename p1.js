91bc5d68a6ba27979f540bebfb5aef6c08ec569b
tguthrie1765 <84353446+tguthrie1765@users.noreply.github.com>
2021-06-04 14:12:22 -0700

    Update p1.js

--- p1.js
+++ p1.js
@@ -4,27 +4,4 @@
 */
 
 console.log(["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][new Date().getDay()])
-```
 
-```js
-/*
- CIT 281 Project 1
- Name: Thomas Guthrie
-*/
-
-// Returns a random number between min (inclusive) and max (exclusive)
-function getRandomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-function getRandomString() {
-    let stringLength = getRandomInteger(5,25)
-    const randomLetters = 'abcdefghijklmnopqrstuvwxyz';
-    let result = '';
-    for ( let i = 0; i < stringLength; i++ ) {
-        result += randomLetters.charAt(getRandomInteger(0,25));
-    }
-    return result;
-}
-
-console.log(String(getRandomString()));
